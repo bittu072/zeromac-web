05be70336fd70201f56df3ad992818a32712d29d
bittu072 <bittu.patel072@gmail.com>
2017-08-12 03:20:04 -0700

    started working on the functionality fo login, registration and logout

--- src/zerousers/views.py
+++ src/zerousers/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+    )
 from django.shortcuts import render
 
 # Create your views here.
+def zero_login(request):
+    return render(request, "example.html", {})
+
+def zero_register(request):
+    return render(request, "example.html", {})
+
+def zero_logout(request):
+    return render(request, "example.html", {})
